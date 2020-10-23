619de8169202793d7fe76f27891c8fb2ff64a54d
kyotov1 <kyotov1@users.noreply.github.com>
2020-10-24 00:30:06 +0300

    Task 2

--- Matrix/Matrix/Program.cs
+++ Matrix/Matrix/Program.cs
@@ -4,10 +4,23 @@ namespace Matrix
 {
     class Program
     {
+        static void PrintMatr(int[,] matr)
+        {
+            for (int row = 0; row < matr.GetLength(0); row++, Console.WriteLine())
+            {
+
+                for (int col = 0; col < matr.GetLength(1); col++)
+                {
+                    Console.Write("{0,3}", matr[col, row]);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Task1();
 
+
+            Task1();
+            Task2();
         }
         /// <summary>
         /// Матрицы. Индексы элементов матриц
@@ -24,11 +37,29 @@ namespace Matrix
             Console.WriteLine("GetUpperBounr(0)" + matr.GetUpperBound(1));
             for (int i = 0; i < matr.GetLength(0); i++, Console.WriteLine())
             {
-                for(int k=0; k<matr.GetLength(1); k++)
+                for (int k = 0; k < matr.GetLength(1); k++)
                 {
                     Console.Write("{0,3}", matr[i, k]);
                 }
             }
+        }
+        /// <summary>
+        /// Matrix from 1 to n, and every rol stars with upper +1
+        /// </summary>
+        static void Task2()
+        {
+            int N;
+            do Console.Write("Write N");
+            while (!int.TryParse(Console.ReadLine(), out N));
+            var matr = new int[N, N];
+            for (int row = 0; row < N; row++)
+            {
+                for (int col = 0; col < N; col++)
+                {
+                    matr[col, row] = (col + row) % N + 1;
+                }
+            }
+            PrintMatr(matr);
         }
     }
 }
