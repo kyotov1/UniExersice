b87ba6cca2f522bfc2cabd576656dbfef853ed84
kyotov1 <kyotov1@users.noreply.github.com>
2020-10-23 23:53:36 +0300

    Some Changes

--- Matrix/Matrix/Program.cs
+++ Matrix/Matrix/Program.cs
@@ -6,8 +6,18 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
+
+
+        }
+        /// <summary>
+        /// Матрицы. Индексы элементов матриц
+        /// </summary>
+        static void Task1()
+        {
+            var matr = new int[3, 4] { { 0, 1, 3, 4 }, { 5, 6, 7, 8 }, { 9, -1, -2, -3 } };
+            Console.WriteLine("Martrix rank " + matr.Rank);
+            Console.WriteLine("All elements " + matr.Length);
+            Console.WriteLine("Matrix type" + matr.GetType());
         }
     }
 }
