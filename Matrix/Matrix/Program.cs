5daee41a61ef70d60cf9405bd69539974abecf7d
kyotov1 <kyotov1@users.noreply.github.com>
2020-10-24 00:07:48 +0300

    Task!()

--- Matrix/Matrix/Program.cs
+++ Matrix/Matrix/Program.cs
@@ -6,7 +6,7 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
-
+            Task1();
 
         }
         /// <summary>
@@ -18,6 +18,17 @@ namespace Matrix
             Console.WriteLine("Martrix rank " + matr.Rank);
             Console.WriteLine("All elements " + matr.Length);
             Console.WriteLine("Matrix type" + matr.GetType());
+            Console.WriteLine("Matrix is fixedSize" + matr.IsFixedSize);
+            Console.WriteLine("GetLenth(1) " + matr.GetLength(1));
+            Console.WriteLine("GetLenth(0)" + matr.GetLength(0));
+            Console.WriteLine("GetUpperBounr(0)" + matr.GetUpperBound(1));
+            for (int i = 0; i < matr.GetLength(0); i++, Console.WriteLine())
+            {
+                for(int k=0; k<matr.GetLength(1); k++)
+                {
+                    Console.Write("{0,3}", matr[i, k]);
+                }
+            }
         }
     }
 }
