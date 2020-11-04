f1bd57b47590c88bd928b92d560d6b763329524e
kyotov1 <kyotov1@users.noreply.github.com>
2020-11-05 01:05:20 +0200

    Update

--- Matrix/Class/Program.cs
+++ Matrix/Class/Program.cs
@@ -4,9 +4,34 @@ namespace Class
 {
     class Program
     {
+        class Telefone
+        {
+            string name;
+            string model;
+            int weight;
+            int diagonal;
+            public Telefone(string name, string model, int weight, int diagonal)
+            {
+                this.name = name;
+                this.model = model;
+                this.
+            }
+            public override string ToString()
+            {
+                return $"Telefone {name}; Model {model}; weight {weight} g.; diagonal {diagonal} inch ";
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Telefone t1 = new Telefone();
+            t1.model = "Samsung";
+            t1.model = "S20";
+            t1.weight = 150;
+            t1.diagonal = 10;
+            Console.WriteLine(t1.ToString());
+
+
         }
     }
 }
