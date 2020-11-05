6a3ff59570ef2fb7a8ebee4a93010d3423ab2278
kyotov1 <kyotov1@users.noreply.github.com>
2020-11-05 12:07:30 +0200

    Seminar update

--- Matrix/Class/Program.cs
+++ Matrix/Class/Program.cs
@@ -10,11 +10,28 @@ namespace Class
             string model;
             int weight;
             int diagonal;
+            public int Weight
+            {
+                get
+                {
+                    return weight;
+                }
+                set
+                {
+                    if (value > 500 || value < 0)
+                        throw new ArgumentException("Not correct weight");
+                    else
+                    {
+                        weight = value;
+                    }
+                }
+            }
             public Telefone(string name, string model, int weight, int diagonal)
             {
                 this.name = name;
                 this.model = model;
-                this.
+               this.weight= weight;
+                this.diagonal = diagonal;
             }
             public override string ToString()
             {
@@ -24,14 +41,20 @@ namespace Class
         }
         static void Main(string[] args)
         {
-            Telefone t1 = new Telefone();
-            t1.model = "Samsung";
-            t1.model = "S20";
-            t1.weight = 150;
-            t1.diagonal = 10;
+            Telefone t1 = new Telefone("Samsung", "A20",1630, 6);
+            //t1.Weight = 1600;
             Console.WriteLine(t1.ToString());
+            Telefone[] tel = new Telefone[] {
+                new Telefone("Samsung", "S20", 200,6),
+                new Telefone("Xiaomi", "A20", 100,5)            
+            };
 
-
+            int w = 0;
+            foreach (Telefone tels in tel)
+            {
+                w += tels.Weight;
+            }
+            Console.WriteLine(w);
         }
     }
 }
